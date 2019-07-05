7d0dfa1b2ada8b7bafd68f6044270038a8cac734
wshzd <wshzd007@hotmail.com>
2019-07-05 18:14:52 +0800

    Update data_helpers.py

--- TextClassification/benchmark/utils/data_helpers.py
+++ TextClassification/benchmark/utils/data_helpers.py
@@ -81,3 +81,27 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
             start_index = batch_num * batch_size
             end_index = min((batch_num + 1) * batch_size, data_size)
             yield data[start_index:end_index]
+            
+            
+            
+def build_vocabulary_and_split_dataset(x_text,dev_sample_index):
+    # Build vocabulary
+    max_document_length = max([len(x.split(" ")) for x in x_text])
+    # vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+    vocab_processor = learn.preprocessing.VocabularyProcessor(100)
+    x = np.array(list(vocab_processor.fit_transform(x_text)))
+    # Randomly shuffle data
+    np.random.seed(10)
+    shuffle_indices = np.random.permutation(np.arange(len(y)))
+    x_shuffled = x[shuffle_indices]
+    y_shuffled = np.array(y)[shuffle_indices]
+
+    # Split train/test set
+    # TODO: This is very crude, should use cross-validation
+    dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
+    x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
+    y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
+    print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
+    print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+
+    print('vocabulary大小', len(vocab_processor.vocabulary_))
