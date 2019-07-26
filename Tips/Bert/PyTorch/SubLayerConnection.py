8a1c85903c32768729b7511d286cec6c8f9161b6
wshzd <wshzd007@hotmail.com>
2019-07-26 15:15:06 +0800

    Create SubLayerConnection.py

---
+++ Tips/Bert/PyTorch/SubLayerConnection.py
@@ -0,0 +1,13 @@
+class SublayerConnection(nn.Module):
+
+    def __init__(self, size, dropout):
+
+        super(SublayerConnection, self).__init__()
+
+        self.norm = LayerNorm(size)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+
+        return x + self.dropout(sublayer(self.norm(x)))
