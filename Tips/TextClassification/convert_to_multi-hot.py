94372aeb38ae2ee4965ef886ef13f6ada42a8181
wshzd <22784759+wshzd@users.noreply.github.com>
2019-09-24 15:51:34 +0800

    Create convert_to_multi-hot.py

---
+++ Tips/TextClassification/convert_to_multi-hot.py
@@ -0,0 +1,16 @@
+def transform_multilabel_as_multihot(label_list,label_size):
+    """
+    convert to multi-hot style
+    :param label_list: e.g.[0,1,4], here 4 means in the 4th position it is true value(as indicate by'1')
+    :param label_size: e.g.199
+    :return:e.g.[1,1,0,1,0,0,........]
+    """
+    result=np.zeros(label_size)
+    #set those location as 1, all else place as 0.
+    result[label_list] = 1
+    return result
+
+label_list=[0,1,2,10]
+label_size=20
+label_list_sparse=transform_multilabel_as_multihot(label_list,label_size)
+print("label_list_sparse:",label_list_sparse)
