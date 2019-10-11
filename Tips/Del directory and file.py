68ae0769031c10e32019a26d55c16780e3749410
wshzd <w-s-h-z-d@163.com>
2019-10-11 11:45:05 +0800

    Create Del directory and file.py

---
+++ Tips/Del directory and file.py
@@ -0,0 +1,16 @@
+        if os.path.exists(FLAGS.output_dir):
+            def del_file(path):
+                ls = os.listdir(path)
+                for i in ls:
+                    c_path = os.path.join(path, i)
+                    if os.path.isdir(c_path):
+                        del_file(c_path)
+                    else:
+                        os.remove(c_path)
+
+            try:
+                del_file(FLAGS.output_dir)
+            except Exception as e:
+                print(e)
+                print('pleace remove the files of output dir and data.conf')
+                exit(-1)
