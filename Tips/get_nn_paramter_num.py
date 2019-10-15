8e967615540f056232b98faa179492baaf3e9c4d
wshzd <w-s-h-z-d@163.com>
2019-10-15 14:21:30 +0800

    Create get_nn_paramter_num.py

---
+++ Tips/get_nn_paramter_num.py
@@ -0,0 +1,18 @@
+def get_total_parameters():
+    """
+    get total parameters of a graph
+    :return:
+    """
+    total_parameters = 0
+    for variable in tf.trainable_variables():
+        # shape is an array of tf.Dimension
+        shape = variable.get_shape()
+        # print(shape)
+        # print(len(shape))
+        variable_parameters = 1
+        for dim in shape:
+            # print(dim)
+            variable_parameters *= dim.value
+        # print(variable_parameters)
+        total_parameters += variable_parameters
+    return total_parameters
